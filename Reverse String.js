9269cd11be4d4aecde4bd70f9d476e09cb8b2b31
yneskladovae <yneskladovae27@gmail.com>
2023-05-20 21:13:28 +0300

    complete - Reverse String

---
+++ Reverse String.js
@@ -0,0 +1,10 @@
+/* 
+344. Reverse String
+https://leetcode.com/problems/reverse-string
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+*/
+
+const reverseString = function(s) {
+    return s.reverse().join('');
+};
